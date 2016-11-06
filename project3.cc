f01133dd9f6479d7a2ded9f2561791bd52314712
Kelton J. Hislop <khislop@bb136-05.adit.mines.edu>
2016-11-06 15:07:23 -0700

    Both ReAcks and Timeouts apear to eb working.

--- project3.cc
+++ project3.cc
@@ -16,18 +16,45 @@
 
 simulator *simulation;
 
+int timer = 500;
+
+char data[500][20];
+
+int aNum = 0;
+//int bAckNum = 0;
+int aAckedNum = 0;
+int bRNum = 0;
+int aReAckNum = 0;
+int timerNum = 0;
+
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message)
 {
-  std::cout << "A side has recieved a message from the application that should be sent to side B: " << message.data << std::endl;
-  pkt packet;
-  packet.seqnum = 0;
-  packet.acknum = 0;
-  packet.checksum = 0;
-  snprintf(packet.payload, 20, "%s", message.data);
-  
-  simulation->tolayer3(A, packet);
-  
+    std::cout << "A side has recieved a message from the application that should be sent to side B: " << message.data << std::endl;
+    std::cout << "Sequence num = " << aNum << std::endl;
+    
+    //Add message to the buffer
+    snprintf(data[aNum], 20, "%s", message.data);
+    
+    //Create the packet
+    pkt packet;
+    packet.seqnum = aNum;
+    packet.acknum = 0;
+    packet.checksum = 0;
+    snprintf(packet.payload, 20, "%s", message.data);
+    //snprintf(packet.payload, 20, "%s", data[aNum]);
+
+    //Pass the packet down to the network
+    simulation->tolayer3(A, packet);
+
+    //If there are no packages in flight, there should be no timer and we need to start one
+    if(aAckedNum == aNum){
+        simulation->starttimer(A, timer);
+    }
+
+    //Increment sequence number
+    aNum += 1;
+
   
 
 
@@ -36,22 +63,66 @@ void A_output(struct msg message)
 void B_output(struct msg message)  /* need be completed only for extra credit */
 {
   std::cout << "B side has recieved a message from the application that should be sent to sideA: " << message.data << std::endl;
+  
 
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(struct pkt packet)
 {
-  std::cout << "A side has recieved a packet sent over the network from side B:" << packet.payload << std::endl;
-
+    std::cout << "A side has recieved a packet sent over the network from side B: " << packet.payload << std::endl;
+    int reAck;
+    //If the incoming ack is greater than the max acked num then update the acked number. 
+    if(packet.acknum > aAckedNum){
+	    aAckedNum = packet.acknum;
+	    aReAckNum = 0;
+	    std::cout << "Acked new message, ";
+	    
+	    //Stop timer and restart it if there are still messages in flight
+	    simulation->stoptimer(A);
+	    if(aAckedNum < aNum){
+	        simulation->starttimer(A, timer);
+	    }
+    }else{
+        aReAckNum += 1;
+        reAck = packet.acknum;
+        std::cout << "ReAcked old message, ";
+    }
+    std::cout << "Current Acked package: " << aAckedNum << std::endl;
+    std::cout << "Current Number of ReAcked packages: " << aReAckNum << std::endl;
+    
+    if(aReAckNum > 3){
+        //Go back N and resend all the data up to that point
+        int oldNum = aNum;
+        aNum = reAck;
+        for(int i = reAck; i < oldNum; i++){
+            msg message;
+            snprintf(message.data, 20, "%s", data[i]);
+            A_output(message);
+        }
+        
+        aReAckNum = 0;
+    }
 }
 
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-
-  std::cout << "Side A's timer has gone off " << std::endl;
+    std::cout << "Side A's timer has gone off " << std::endl;
+    
+    //Go back N and resend all the data up to that point
+    int oldNum = aNum;
+    aNum = aAckedNum;
+    for(int i = aAckedNum; i < oldNum; i++){
+        msg message;
+        snprintf(message.data, 20, "%s", data[i]);
+        A_output(message);
+    }
+    
+    if(aAckedNum < aNum){
+	        simulation->starttimer(A, timer);
+    }
 }  
 
 /* the following routine will be called once (only) before any other */
@@ -64,13 +135,23 @@ void A_init()
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
-    std::cout << "B side has recieved a packet sent over the network from side A:" << packet.payload << std::endl;
-  pkt packet2;
-  packet2.seqnum = 0;
-  packet2.acknum = 0;
-  packet2.checksum = 0;
-  snprintf(packet2.payload, 7, "%s", "got it");
-  simulation->tolayer3(B, packet2);
+    std::cout << "B side has recieved a packet sent over the network from side A: " << packet.payload << std::endl;
+    pkt packet2;
+    if(packet.seqnum == bRNum){
+        packet2.seqnum = 0;
+        packet2.acknum = packet.seqnum + 1;
+        packet2.checksum = 0;
+        snprintf(packet2.payload, 4, "%s", "ACK");
+        simulation->tolayer3(B, packet2);
+        bRNum += 1;
+    }else{
+        packet2.seqnum = 0;
+        packet2.acknum = bRNum;
+        packet2.checksum = 0;
+        snprintf(packet2.payload, 5, "%s", "NACK");
+        simulation->tolayer3(B, packet2);
+
+    }
     
 
 }
